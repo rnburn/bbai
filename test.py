ef53ceef1db3aadd3316ebdde9f3ef9af2970bda
Ryan Burn <ryan.burn@gmail.com>
2020-06-09 20:44:47 -0700

    Add test

---
+++ test.py
@@ -0,0 +1,20 @@
+import peak_engines
+import numpy as np
+np.random.seed(0)
+
+n = 15
+k = 2
+
+def make_random_problem():
+    X = np.random.random_sample((n,k))
+    for j in range(X.shape[1]):
+        col = X[:, j]
+        X[:, j] = (col - np.mean(col)) / np.std(col)
+    beta = np.array([1.0, 0.0])
+    y = np.dot(X, beta) + np.random.normal(size=n)
+    return X, y
+
+X, y = make_random_problem()
+model = peak_engines.WarpedLinearRegressionModel()
+model.fit(X, y)
+print(model.regressors)
